cbb88a9fbc07828510e2a1eca64a38b739e0688f
Ilnur Yasaviev <ir.yasaviev@yandex.ru>
2019-04-16 15:42:07 +0300

    Убрал лишнее.

--- Swishark/Controllers/ProjectController.cs
+++ Swishark/Controllers/ProjectController.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Services.Account;
 using Services.Project;
-using Services.ProjectTask;
 using Swishark.Models;
 
 namespace Swishark.Controllers
